952532fb1c047c208b25118799ca6743b5cc3db0
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2022-11-22 21:02:14 -0800

    4

--- pages/components/Ts.ts
+++ pages/components/Ts.ts
@@ -1,18 +1,16 @@
-export type Permission = {
+export default interface Permission {
   permission_ID: number;
   permission_section: string;
   permission_section_code: string;
   permission_item: string;
   permission_item_code: string;
   permission_item_description: string;
-};
+}
 
-export type Prop_Permission = {
+export default interface Prop_Permission {
   permission: Permission;
-};
+}
 
-export type Array_Permission = {
+export default interface Array_Permission {
   array_Permission: Permission[];
-};
-
-export default Permission;
+}
