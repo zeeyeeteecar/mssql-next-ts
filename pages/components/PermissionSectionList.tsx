ee89b3f6830924710b64c2c835bd03a9136b06f1
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2022-11-23 21:07:07 -0800

    2

--- pages/components/PermissionSectionList.tsx
+++ pages/components/PermissionSectionList.tsx
@@ -1,10 +1,14 @@
-
-
 export const options = [
   { value: "payment", text: "payment" },
   { value: "Mem man", text: "Mem man" },
   { value: "Donor", text: "Donor" },
   { value: "vault", text: "vault" },
 ];
+
+import React from "react";
+
+export default function PermissionSectionList() {
+  return <div>PermissionSectionList</div>;
+}
 
 //export default options
